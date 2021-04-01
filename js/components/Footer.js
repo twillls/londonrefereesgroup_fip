09af645dddf0871f3a23b8e4433232e482fa76f4
Haley Billson <h_billson@fanshaweonline.ca>
2021-03-31 21:14:28 -0400

    added router-links to footer navigation

--- js/components/Footer.js
+++ js/components/Footer.js
@@ -11,40 +11,102 @@ export default {
     },
 
     template: 
-    `<section id="main-footer">
+    `<secte id="main-footer">
     <div class="left">
         <div class="column">
-            <p @click="setView" name="landing" class="biglink">Home</p></li>
-            <p @click="setView"name="about" class="biglink">About Us</p>
-            <p @click="setView"name="story" class="smalllink">Our Story</p>
-            <p @click="setView"name="partners" class="smalllink">Our Partners</p>
-            <p @click="setView"name="message" class="smalllink">A Message from the President</p>
-            <p @click="setView"name="contact" class="smalllink">Contact Us</p>
+            <router-link to="" v-slot="{href, route, navigate}" custom>
+                <p :href="href" @click="navigate" name="landing" class="biglink">Home</p></li>
+            </router-link>
+            <router-link to="/about" v-slot="{href, route, navigate}" custom>
+                <p :href="href" @click="navigate" name="about" class="biglink">About Us</p>
+            </router-link>
+
+            <router-link to="/story" v-slot="{href, route, navigate}" custom>
+                <p :href="href" @click="navigate" name="story" class="smalllink">Our Story</p>
+            </router-link>
+
+            <router-link to="/partners" v-slot="{href, route, navigate}" custom>
+                <p :href="href" @click="navigate" class="smalllink">Our Partners</p>
+            </router-link>
+
+            <router-link to="/message" v-slot="{href, route, navigate}" custom>
+                <p :href="href" @click="navigate" class="smalllink">A Message from the President</p>
+            </router-link>
+
+            <router-link to="/contact" v-slot="{href, route, navigate}" custom>
+                <p :href="href" @click="navigate" class="smalllink">Contact Us</p>
+            </router-link>
+
         </div>
         <div class="column">
-            <p @click="setView"name="services" class="biglink">Services</p>
-            <p @click="setView"name="members" class="smalllink">Membership</p>
-            <p @click="setView"name="training" class="smalllink">Training</p>
-            <p@click="setView" name="booking" class="smalllink">Booking</p>
+        <router-link to="/services" v-slot="{href, route, navigate}" custom>
+            <p :href="href" @click="navigate" name="services" class="biglink">Services</p>
+            </router-link>
+
+            <router-link to="/membership" v-slot="{href, route, navigate}" custom>
+            <p :href="href" @click="navigate" class="smalllink">Membership</p>
+            </router-link>
+
+            <router-link to="/skills" v-slot="{href, route, navigate}" custom>
+            <p :href="href" @click="navigate"  class="smalllink">Training</p>
+            </router-link>
+
+            <router-link to="/booking" v-slot="{href, route, navigate}" custom>
+            <p :href="href" @click="navigate" class="smalllink">Booking</p>
+            </router-link>
+
         </div>
         <div class="column">
-            <p @click="setView"name="members"class="biglink">Membership</p>
-            <p @click="setView"name="structure"class="smalllink">Committee Structure</p>
-            <p @click="setView"name="certification"class="smalllink">Partner Certification Clinics</p>
-            <p @click="setView"name="skills"class="smalllink">Skill Building Workshops</p>
-            <p @click="setView"name="scheduling"class="smalllink">Scheduling</p>
-            <p @click="setView"name="gallery"class="smalllink">Gallery</p>
+        <router-link to="/membership" v-slot="{href, route, navigate}" custom>
+            <p :href="href" @click="navigate" class="biglink">Membership</p>
+            </router-link>
+
+            <router-link to="/stucture" v-slot="{href, route, navigate}" custom>
+            <p :href="href" @click="navigate" class="smalllink">Committee Structure</p>
+            </router-link>
+
+            <router-link to="/certification" v-slot="{href, route, navigate}" custom>
+            <p :href="href" @click="navigate" class="smalllink">Partner Certificate Clinics</p>
+            </router-link>
+
+            <router-link to="/skills" v-slot="{href, route, navigate}" custom>
+            <p :href="href" @click="navigate" class="smalllink">Skill Building Workshops</p>
+            </router-link>
+
+            <router-link to="/scheduling" v-slot="{href, route, navigate}" custom>
+            <p :href="href" @click="navigate" class="smalllink">Scheduling</p>
+            </router-link>
+
+            <router-link to="/gallery" v-slot="{href, route, navigate}" custom>
+            <p :href="href" @click="navigate" class="smalllink">Gallery</p>
+            </router-link>
+
         </div>
         <div class="column">
-            <p @click="setView"name="jr"class="biglink">Junior Membership Program</p>
-            <p @click="setView"name="partners"class="biglink">Our Partners</p>
-            <p @click="setView"name="edu"class="biglink">Education</p>
-            <p @click="setView"name="booking"class="biglink">Booking Information</p>
+        <router-link to="/juniors" v-slot="{href, route, navigate}" custom>
+            <p :href="href" @click="navigate" class="biglink">Junior Membership Program</p>
+            </router-link>
+
+            <router-link to="/partners" v-slot="{href, route, navigate}" custom>
+            <p :href="href" @click="navigate" class="biglink">Our Partners</p>
+            </router-link>
+
+            <router-link to="/education" v-slot="{href, route, navigate}" custom>
+            <p :href="href" @click="navigate" class="biglink">Education</p>
+            </router-link>
+
+            <router-link to="/booking" v-slot="{href, route, navigate}" custom>
+            <p :href="href" @click="navigate" class="biglink">Booking Information</p>
+            </router-link>
+
         </div>
     </div>
     <div class="right">
         <div class="column">
-            <p @click="setView"name="contact"class="biglink">CONTACT US</p>
+        <router-link to="/contact" v-slot="{href, route, navigate}" custom>
+            <p :href="href" @click="navigate" class="biglink">CONTACT US</p>
+            </router-link>
+
             <p class="contact-info">
                 lrg@londonrefereesgroup.com <br>
                 (555) 555-5555 <br>
@@ -59,7 +121,7 @@ export default {
         </div>
     </div>
     <div class="copy">[COPYRIGHT]</div>
-</section>`,
+</secte>`,
 
     methods: {
         setView(e) {
